a3fce3ba4b4ffc30ae534073abbe79302976f44c
XjuKam <165759241+XjuKam@users.noreply.github.com>
2024-04-07 23:43:57 +0300

    V2
    Увеличил мапу до 100
    убрал лишние проверки
    изменил приоритетность условий
    вынес свитч в функцию

--- calculator.go
+++ calculator.go
@@ -8,68 +8,125 @@ import (
 	"strings"
 )
 
-/*
-1. принимаю на вход значение
-2. убираю пробелы
-4. разделяю значения на 3 переменные
-5. проверяю 1 и 3 переменные на возможность изменения типа на int (это можно, так как при конвертации стринг (римские) будет 0, а на вход нельзя 0)
-    1.  если инт = арабские - идем дальше
-    2. если float - выдаем панику
-    3. если стринг
-        1. дополнительная проверка на соответствие значений в мапе римских цифер
-        2. конвертируем их в арабские
-    4. если 1 и 3 переменны имеют разные типы - выдаем панику
-    5. проверяем переменные на ограничения от 1 до 10
-6. используем свитч по операцияю +*-/
-1. если ок - выводим результат
-1. если были римские - идем в мапу для получения соответсвующего ключа значению результата
-2. если нет - выдаем панику
-
-*/
-
 func main() {
 
-	var v1 string
-	var v2 string
-	var v3 string
-	var v1int int
-	var v3int int
+	var partOne string
+	var operator string
+	var partTwo string
+	var partOneInt int
+	var partTwoInt int
 	var romeOper bool
 	var result int
 
 	// карта допустимых значений на вход
 	mm := map[string]data{
-		"one":       {"I", 1},
-		"two":       {"II", 2},
-		"three":     {"III", 3},
-		"four":      {"IV", 4},
-		"five":      {"V", 5},
-		"six":       {"VI", 6},
-		"seven":     {"VII", 7},
-		"eight":     {"VIII", 8},
-		"nine":      {"IX", 9},
-		"ten":       {"X", 10},
-		"eleven":    {"XI", 11},
-		"twelve":    {"XII", 12},
-		"thirteen":  {"XIII", 13},
-		"fourteen":  {"XIV", 14},
-		"fifteen":   {"XV", 15},
-		"sixteen":   {"XVI", 16},
-		"seventeen": {"XVII", 17},
-		"eighteen":  {"XVIII", 18},
-		"nineteen":  {"XIX", 19},
-		"twenty":    {"XX", 20},
+		"four":         {"IV", 4},
+		"five":         {"V", 5},
+		"six":          {"VI", 6},
+		"seven":        {"VII", 7},
+		"eight":        {"VIII", 8},
+		"nine":         {"IX", 9},
+		"ten":          {"X", 10},
+		"eleven":       {"XI", 11},
+		"twelve":       {"XII", 12},
+		"thirteen":     {"XIII", 13},
+		"fourteen":     {"XIV", 14},
+		"fifteen":      {"XV", 15},
+		"sixteen":      {"XVI", 16},
+		"seventeen":    {"XVII", 17},
+		"eighteen":     {"XVIII", 18},
+		"nineteen":     {"XIX", 19},
+		"twenty":       {"XX", 20},
+		"twentyone":    {"XXI", 21},
+		"twentytwo":    {"XXII", 22},
+		"twentythree":  {"XXIII", 23},
+		"twentyfour":   {"XXIV", 24},
+		"twentyfive":   {"XXV", 25},
+		"twentysix":    {"XXVI", 26},
+		"twentyseven":  {"XXVII", 27},
+		"twentyeight":  {"XXVIII", 28},
+		"twentynine":   {"XXIX", 29},
+		"thirty":       {"XXX", 30},
+		"thirtyone":    {"XXXI", 31},
+		"thirtytwo":    {"XXXII", 32},
+		"thirtythree":  {"XXXIII", 33},
+		"thirtyfour":   {"XXXIV", 34},
+		"thirtyfive":   {"XXXV", 35},
+		"thirtysix":    {"XXXVI", 36},
+		"thirtyseven":  {"XXXVII", 37},
+		"thirtyeight":  {"XXXVIII", 38},
+		"thirtynine":   {"XXXIX", 39},
+		"forty":        {"XL", 40},
+		"fortyone":     {"XLI", 41},
+		"fortytwo":     {"XLII", 42},
+		"fortythree":   {"XLIII", 43},
+		"fortyfour":    {"XLIV", 44},
+		"fortyfive":    {"XLV", 45},
+		"fortysix":     {"XLVI", 46},
+		"fortyseven":   {"XLVII", 47},
+		"fortyeight":   {"XLVIII", 48},
+		"fortynine":    {"XLIX", 49},
+		"fifty":        {"L", 50},
+		"fiftyone":     {"LI", 51},
+		"fiftytwo":     {"LII", 52},
+		"fiftythree":   {"LIII", 53},
+		"fiftyfour":    {"LIV", 54},
+		"fiftyfive":    {"LV", 55},
+		"fiftysix":     {"LVI", 56},
+		"fiftyseven":   {"LVII", 57},
+		"fiftyeight":   {"LVIII", 58},
+		"fiftynine":    {"LIX", 59},
+		"sixty":        {"LX", 60},
+		"sixtyone":     {"LXI", 61},
+		"sixtytwo":     {"LXII", 62},
+		"sixtythree":   {"LXIII", 63},
+		"sixtyfour":    {"LXIV", 64},
+		"sixtyfive":    {"LXV", 65},
+		"sixtysix":     {"LXVI", 66},
+		"sixtyseven":   {"LXVII", 67},
+		"sixtyeight":   {"LXVIII", 68},
+		"sixtynine":    {"LXIX", 69},
+		"seventy":      {"LXX", 70},
+		"seventyone":   {"LXXI", 71},
+		"seventytwo":   {"LXXII", 72},
+		"seventythree": {"LXXIII", 73},
+		"seventyfour":  {"LXXIV", 74},
+		"seventyfive":  {"LXXV", 75},
+		"seventysix":   {"LXXVI", 76},
+		"seventyseven": {"LXXVII", 77},
+		"seventyeight": {"LXXVIII", 78},
+		"seventynine":  {"LXXIX", 79},
+		"eighty":       {"LXXX", 80},
+		"eightyone":    {"LXXXI", 81},
+		"eightytwo":    {"LXXXII", 82},
+		"eightythree":  {"LXXXIII", 83},
+		"eightyfour":   {"LXXXIV", 84},
+		"eightyfive":   {"LXXXV", 85},
+		"eightysix":    {"LXXXVI", 86},
+		"eightyseven":  {"LXXXVII", 87},
+		"eightyeight":  {"LXXXVIII", 88},
+		"eightynine":   {"LXXXIX", 89},
+		"ninety":       {"XC", 90},
+		"ninetyone":    {"XCI", 91},
+		"ninetytwo":    {"XCII", 92},
+		"ninetythree":  {"XCIII", 93},
+		"ninetyfour":   {"XCIV", 94},
+		"ninetyfive":   {"XCV", 95},
+		"ninetysix":    {"XCVI", 96},
+		"ninetyseven":  {"XCVII", 97},
+		"ninetyeight":  {"XCVIII", 98},
+		"ninetynine":   {"XCIX", 99},
+		"hundred":      {"C", 100},
 	}
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Input")
 		text, _ := reader.ReadString('\n') // Ждет ввода данных в формате строки
 		text = strings.TrimSpace(text)     // Очищает все пустоты (пробелы, табуляцию)
-		fmt.Println(text)
 
 		// Ищем индекс знака операции
 		opIndex := FindOperatorIndex(text)
-		fmt.Println(text, opIndex)
+
 		// Проверяем, что знак операции найден
 		if opIndex == -1 {
 			fmt.Println("Знак операции не найден в выражении")
@@ -77,107 +134,94 @@ func main() {
 		}
 
 		// Извлекаем значения введенных переменных
-		v1 = text[:opIndex]
-		v2 = text[opIndex : opIndex+1]
-		v3 = text[opIndex+1:]
-		fmt.Println(text, opIndex, v1, v2, v3)
-		v1 = strings.TrimSpace(v1) // Очищает все пустоты (пробелы, табуляцию)
-		v2 = strings.TrimSpace(v2) // Очищает все пустоты (пробелы, табуляцию)
-		v3 = strings.TrimSpace(v3) // Очищает все пустоты (пробелы, табуляцию)
+		partOne = text[:opIndex]
+		operator = text[opIndex : opIndex+1]
+		partTwo = text[opIndex+1:]
 
-		v1int, _ = strconv.Atoi(v1) // Преобразование строки в число
-		v3int, _ = strconv.Atoi(v3) // Преобразование строки в число
-		fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int)
+		partOne = strings.TrimSpace(partOne)   // Очищает все пустоты (пробелы, табуляцию)
+		operator = strings.TrimSpace(operator) // Очищает все пустоты (пробелы, табуляцию)
+		partTwo = strings.TrimSpace(partTwo)   // Очищает все пустоты (пробелы, табуляцию)
+
+		partOneInt, _ = strconv.Atoi(partOne) // Преобразование строки в число
+		partTwoInt, _ = strconv.Atoi(partTwo) // Преобразование строки в число
 
 		// Проверяем систему нумерации
-		if OnlyArabRome(v1int, v3int) == "rome" {
-			key, found := GetKeyByValueRome(mm, v1)
+		if OnlyArabRome(partOneInt, partTwoInt) == "rome" {
+			key, found := GetKeyByValueRome(mm, partOne)
 			valueMm := mm[key]
-			fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int, found)
-			if found {
-				v1int = valueMm.arabNum
-				fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int, found, valueMm, valueMm.arabNum)
+			if found == true {
+				partOneInt = valueMm.arabNum
 			} else {
 				panic("Выдача паники. Недопустимое значение, только 1-10, либо I - X")
 			}
-			key, found = GetKeyByValueRome(mm, v3)
+			key, found = GetKeyByValueRome(mm, partTwo)
 			valueMm = mm[key]
 			if found {
-				v3int = valueMm.arabNum
-				fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int, found, valueMm, valueMm.arabNum)
+				partTwoInt = valueMm.arabNum
 				romeOper = true
 			} else {
 				panic("Выдача паники, так как используются одновременно разные системы счисления.")
 			}
-
-			fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int, found, valueMm, valueMm.arabNum, romeOper)
-			// Выполнение операции
-
-			switch v2 {
-			case "+":
-				result = v1int + v3int
-			case "-":
-				result = v1int - v3int
-			case "*":
-				result = v1int * v3int
-			case "/":
-				if v3int != 0 {
-					result = v1int / v3int
-				} else {
-					fmt.Println("Деление на ноль недопустимо.")
+			result = GetOperationResult(operator, partOneInt, partTwoInt)
+			/*
+				// Выполнение операции
+				switch operator {
+				case "+":
+					result = partOneInt + partTwoInt
+				case "-":
+					result = partOneInt - partTwoInt
+				case "*":
+					result = partOneInt * partTwoInt
+				case "/":
+					if partTwoInt != 0 {
+						result = partOneInt / partTwoInt
+					} else {
+						fmt.Println("Деление на ноль недопустимо.")
+						return
+					}
+				default:
+					fmt.Println("Неверная операция.")
 					return
 				}
-			default:
-				fmt.Println("Неверная операция.")
-				return
-			}
-			fmt.Println(text, opIndex, v1, v2, v3, v1int, v3int, found, valueMm, valueMm.arabNum, romeOper, result)
-			if result <= -1 && OnlyArabRome(v1int, v3int) == "rome" {
-				panic("В риме не было отрицательных, ну вот так и жили.")
-			} else if romeOper == true {
+			*/
+
+			if result <= 0 && romeOper == true {
+				panic("В риме не было отрицательных и нуля, ну вот так и жили.")
+			} else {
 				key, _ := GetKeyByValueArab(mm, result)
 				valueMm := mm[key]
 				resultRome := valueMm.romeNum
-				fmt.Println(key, valueMm)
 				fmt.Printf("Output:\n%v\n", resultRome)
-			} else {
-				fmt.Printf("Output:\n%v\n", result)
 			}
 		} else {
+			result = GetOperationResult(operator, partOneInt, partTwoInt)
 
-			// Выполнение операции
-			switch v2 {
-			case "+":
-				result = v1int + v3int
-			case "-":
-				result = v1int - v3int
-			case "*":
-				result = v1int * v3int
-			case "/":
-				if v3int != 0 {
-					result = v1int / v3int
-				} else {
-					fmt.Println("Деление на ноль недопустимо.")
+			/*
+				// Выполнение операции
+				switch operator {
+				case "+":
+					result = partOneInt + partTwoInt
+				case "-":
+					result = partOneInt - partTwoInt
+				case "*":
+					result = partOneInt * partTwoInt
+				case "/":
+					if partTwoInt != 0 {
+						result = partOneInt / partTwoInt
+					} else {
+						fmt.Println("Деление на ноль недопустимо.")
+						return
+					}
+				default:
+					fmt.Println("Неверная операция.")
 					return
 				}
-			default:
-				fmt.Println("Неверная операция.")
-				return
-			}
-
-			if result <= -1 && OnlyArabRome(v1int, v3int) == "rome" {
-				panic("В риме не было отрицательных, ну вот так и жили.")
-			} else if romeOper == true {
-				key, _ := GetKeyByValueRome(mm, strconv.Itoa(result))
-				valueMm := mm[key]
-				resultRome := valueMm.romeNum
-				fmt.Printf("Output:\n%v", resultRome)
-			} else {
-				fmt.Printf("Output:\n%v\n", result)
-			}
+			*/
+			fmt.Printf("Output:\n%v\n", result)
 		}
 	}
 }
+
 func FindOperatorIndex(input string) int {
 	operators := "+-*/"
 
@@ -190,17 +234,14 @@ func FindOperatorIndex(input string) int {
 	return -1
 }
 
-func OnlyArabRome(v1int, v3int int) string {
-	if v1int == 0 || v3int == 0 {
-		//	panic("Выдача паники, два операнда должны быть только  1 - 10, либо  I - X.")
-		return "rome"
-	} else if v1int >= 1 && v1int <= 10 && v3int >= 1 && v3int <= 10 {
+func OnlyArabRome(partOneInt, partTwoInt int) string {
+	if partOneInt >= 1 && partOneInt <= 10 && partTwoInt >= 1 && partTwoInt <= 10 {
 		return "arab"
-	} else if v1int >= 1 && v3int == 0 {
+	} else if partOneInt >= 1 && partTwoInt == 0 {
 		panic("Выдача паники, так как используются одновременно разные системы счисления.")
-	} else if v1int == 0 && v3int >= 1 {
+	} else if partOneInt == 0 && partTwoInt >= 1 {
 		panic("Выдача паники, так как используются одновременно разные системы счисления.")
-	} else if v1int >= 11 || v3int >= 11 {
+	} else if partOneInt >= 11 || partTwoInt >= 11 {
 		panic("Выдача паники, два операнда должны быть только  1 - 10, либо  I - X.")
 	} else {
 		return "rome"
@@ -228,4 +269,28 @@ func GetKeyByValueArab(mm map[string]data, valueInt int) (string, bool) {
 		}
 	}
 	return "", false
+}
+
+func GetOperationResult(operator string, partOneInt int, partTwoInt int) int {
+	// Выполнение операции
+	switch operator {
+	case "+":
+		result := partOneInt + partTwoInt
+		return result
+	case "-":
+		result := partOneInt - partTwoInt
+		return result
+	case "*":
+		result := partOneInt * partTwoInt
+		return result
+	case "/":
+		if partTwoInt != 0 {
+			result := partOneInt / partTwoInt
+			return result
+		} else {
+			panic("Деление на ноль недопустимо.")
+		}
+	default:
+		panic("Неверная операция.")
+	}
 }
